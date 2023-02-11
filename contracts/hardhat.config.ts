5f55d59e6b125b8d9d05747e65ee6c952941522d
Dinesh <dineshaitham2@gmail.com>
2023-02-11 20:00:00 +0530

    added push notifications

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -3,6 +3,11 @@ import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-secure-accounts";
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    mumbai: {
+      url: "https://dimensional-fabled-patron.matic-testnet.discover.quiknode.pro/52f7211c36c11560b648d2af0576acb8e9fb8062/",
+    },
+  },
 };
 
 export default config;
